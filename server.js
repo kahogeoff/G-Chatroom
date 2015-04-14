778a22ed2da44fb9e4f66dbd14cfcb49155204b0
Geoffrey Cheung <kahogeoff@gmail.com>
2015-04-15 02:48:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -85,6 +85,6 @@ io.on('connection', function (socket) {
     });
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(8080, function () {
+    console.log('listening on *:8080');
 });
