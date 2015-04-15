00661f73d73bb357ddd4fb4f523091825acfb111
Geoffrey Cheung <kahogeoff@gmail.com>
2015-04-15 20:15:13 +0800

    Add environment variable setting

--- server.js
+++ server.js
@@ -4,30 +4,43 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var crc = require('crc');
 var mongo = require('mongodb').MongoClient;
+var crc = require('crc');
+
+var server_port = process.env.PORT || 8080;
+
+var mongodb_host = process.env.MONGODB_ADDON_HOST || '127.0.0.1';
+var mongodb_port = process.env.MONGODB_ADDON_PORT || 27017;
+var mongodb_user = process.env.MONGODB_ADDON_USER || 'admin';
+var mongodb_pwd = process.env.MONGODB_ADDON_PASSWORD || 'admin';
+var mongodb_db = process.env.MONGODB_ADDON_DB || 'mydb';
+
 var d = new Date();
 var user_count = 0;
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/app/index.html');
-});
 
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/js', express.static(__dirname + '/app/js'));
 app.use('/css', express.static(__dirname + '/app/css'));
 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/app/index.html');
+});
+
+//When client connected
 io.on('connection', function (socket) {
     var clientIp = socket.request.connection.remoteAddress;
     var n = d.toDateString();
     var id = crc.crc32(clientIp + n).toString(16);
+
+    var mongodb_uri = getMongodbURI (mongodb_host, mongodb_port, mongodb_user, mongodb_pwd, mongodb_db);
 
     console.log('User from:' + clientIp + ' connected.');
     user_count+=1;
     io.emit('get online user',user_count);
     console.log('Online: '+ user_count);
 
-    mongo.connect("mongodb://localhost:27017/gchatroom", function (err, db) {
+    //When DB connected
+    mongo.connect(mongodb_uri, function (err, db) {
         if (err != null) {
             console.log(err.name + ": " + err.message);
             return 1;
@@ -49,6 +62,7 @@ io.on('connection', function (socket) {
         });
     });
 
+    //When client disconnected
     socket.on('disconnect', function () {
         console.log('User from:' + clientIp + ' disconnected');
         user_count-=1;
@@ -59,10 +73,11 @@ io.on('connection', function (socket) {
         }
     });
 
+    //When messages recivied
     socket.on('chat message', function (name, msg, color) {
         var msgd = new Date();
         var n = msgd.getTime();
-        mongo.connect("mongodb://localhost:27017/gchatroom", function (err, db) {
+        mongo.connect(mongodb_uri, function (err, db) {
             var collection = db.collection('chatroom');
             collection.insert({
                 "createdAt": new Date(),
@@ -85,6 +100,14 @@ io.on('connection', function (socket) {
     });
 });
 
-http.listen(8080, function () {
-    console.log('listening on *:8080');
+http.listen(server_port, function () {
+    console.log('listening on *:' + server_port);
 });
+
+function getMongodbURI (mongodb_host, mongodb_port, mongodb_user, mongodb_pwd, mongodb_db) {
+    if(mongodb_user != null && mongodb_pwd != null) {
+        return "mongodb://"+mongodb_user+':'+mongodb_pwd+'@'+mongodb_host+':'+mongodb_port+'/'+mongodb_db;
+    } else {
+        return "mongodb://"+mongodb_host+':'+mongodb_port+'/'+mongodb_db;
+    }
+}
