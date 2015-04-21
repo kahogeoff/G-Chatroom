7f21ca33b752fbf08b40a738ed901cda9236a2fc
Geoffrey Cheung <kahogeoff@gmail.com>
2015-04-21 14:22:56 +0800

    Hot fix

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ io.on('connection', function (socket) {
             }
         });
 
-        io.emit('chat message', n, name, msg, id, color);
+        io.emit('chat message', n, name, security_config.using_aes_to_store? aes.encrypt(msg):msg, id, color);
     });
 });
 
