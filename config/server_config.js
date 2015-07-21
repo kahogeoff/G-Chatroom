ed1cbc3c3668c612fa907ed1586c96ab884efe62
Geoffrey Cheung <geoffreycheung@GeoffreytekiMacBook-Pro.local>
2015-07-21 19:12:14 +0800

    Hot fix

--- config/server_config.js
+++ config/server_config.js
@@ -2,28 +2,28 @@
 
 module.exports = {
   //Set your sever info in here//
-  ip : process.env.IP || process.env.INADDR_ANY,
-  port : process.env.PORT || 8080,
+  ip: process.env.IP || process.env.INADDR_ANY,
+  port: process.env.PORT || 8080,
   //////////////////////////////
 
   //Set your database in here//
-  db_host : process.env.MONGODB_ADDON_HOST || '127.0.0.1',
-  db_port : process.env.MONGODB_ADDON_PORT || 27017,
-  db_user : process.env.MONGODB_ADDON_USER || '',
-  db_pwd : process.env.MONGODB_ADDON_PASSWORD || '',
-  db_name : process.env.MONGODB_ADDON_DB || 'myweb',
-  db_uri : process.env.MONGOLAB_URI || ''; //Heroku MongoLab URI
+  db_host: process.env.MONGODB_ADDON_HOST || '127.0.0.1',
+  db_port: process.env.MONGODB_ADDON_PORT || 27017,
+  db_user: process.env.MONGODB_ADDON_USER || '',
+  db_pwd: process.env.MONGODB_ADDON_PASSWORD || '',
+  db_name: process.env.MONGODB_ADDON_DB || 'myweb',
+  db_uri: process.env.MONGOLAB_URI || '', //Heroku MongoLab URI
   ////////////////////////////
 
-  getMongodbURI: function () {
-      if(this.db_uri != '')
-      {
-        return this.db_uri;
-      }
-      if(this.db_user != '' && this.db_pwd != '') {
-          return "mongodb://"+this.db_user+':'+this.db_pwd+'@'+this.db_host+':'+this.db_port+'/'+this.db_name;
+  getMongodbURI: function() {
+    if (this.db_uri != '') {
+      return this.db_uri;
+    } else {
+      if (this.db_user != '' && this.db_pwd != '') {
+        return "mongodb://" + this.db_user + ':' + this.db_pwd + '@' + this.db_host + ':' + this.db_port + '/' + this.db_name;
       } else {
-          return "mongodb://"+this.db_host+':'+this.db_port+'/'+this.db_name;
+        return "mongodb://" + this.db_host + ':' + this.db_port + '/' + this.db_name;
       }
+    }
   }
 };
